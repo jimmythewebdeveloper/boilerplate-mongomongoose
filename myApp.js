135083716cf068e16a719b081b1699cd0e4cb5a4
jimmy parra <jimmyparra2022@gmail.com>
2023-03-08 21:42:47 -0500

    lesson 2.1 create a model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const personSchema = new Schema({
   name: {
