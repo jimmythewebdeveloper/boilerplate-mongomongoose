1e84ac3a7732d6d1564eb69e6e7c72bba5b689ea
jimmy parra <jimmyparra2022@gmail.com>
2023-03-09 23:31:13 -0500

    lesson 5.0 search with Model.find()

--- myApp.js
+++ myApp.js
@@ -42,7 +42,13 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findOneByFood = (food, done) => {
